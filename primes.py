af86a3cf467cdddb9c581693544e28cda450dcbb
yongji.yang <yongji.yang@kcl.ac.uk>
2022-09-29 22:07:02 +0100

    Now can raise valueErr

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+    prime = 2
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    while len(list) < number_of_primes:
+        list.append(prime)
+        num = prime + 1
+        while not isPrime(num):
+            num += 1
+        prime = num
+
     return list
+
+
+def isPrime(number):
+    counter = 2
+    while counter < number:
+        if number % counter == 0:
+            return False
+        counter += 1
+    return True
